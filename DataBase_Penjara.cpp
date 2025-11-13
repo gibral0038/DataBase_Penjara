2f2184b6afafdff63691a7db773cb67621cdb999
FaizSoleil <faizmnugroho@gmail.com>
2025-11-13 21:42:45 +0700

    added to do lists and added more part of the code

--- DataBase_Penjara.cpp
+++ DataBase_Penjara.cpp
@@ -2,6 +2,14 @@
 #include <fstream>
 using namespace std;
 
+/*
+To-do lists
+ -complete functions
+ -fix up the input functions (change the string inputs to getline)
+ -fix up variable name and add comments
+ -clean up some crowded part of the codes
+*/
+
 string namaFile = "DataTahanan.txt";
 
 struct Date{
@@ -19,15 +27,74 @@ struct Tahanan{
     Date keluar;
 };
 
+void menu();
+void input(Tahanan* list, int& jmlhTahanan);
+void hapus(Tahanan* list, int& jmlhTahanan);
+void cari_tahanan(Tahanan* list, int& jmlhTahanan);
+
+int main(){
+    int jmlhTahanan = 0;
+    fstream file;
+    Tahanan* list = new Tahanan[101];
+    Tahanan* ptr = &list[0];
+    file.open(namaFile);
+    file >> jmlhTahanan;
+    Tahanan* max = ptr + jmlhTahanan;
+    for (ptr; ptr < max; ptr++){
+        file >> ptr->nama >> ptr->noTahanan >> ptr->tmptLahir >> ptr->kejahatan 
+        >> ptr->masuk.tanggal >> ptr->masuk.bulan >> ptr->masuk.tahun 
+        >> ptr->keluar.tanggal >> ptr->keluar.bulan >> ptr->keluar.tahun;
+    }
+    file.close();
+
+    bool check = true;
+    while (check){
+        int pilih;
+        menu();
+        cin >> pilih;
+        switch (pilih){
+            case 1 :
+                input(list, jmlhTahanan);
+                break;
+            case 2 :
+
+            case 3 :
+
+            case 4 :
+                cari_tahanan(list, jmlhTahanan);
+                break;
+            case 5 :
+                hapus(list, jmlhTahanan);
+                break;
+            case 6 :
+                file.open(namaFile, ios::trunc);
+                file << jmlhTahanan << endl;
+                for (int i = 0; i < jmlhTahanan; i++){
+                    file << list[i].nama <<
+                    endl << list[i].noTahanan << " " << list[i].tmptLahir << " " << list[i].kejahatan << 
+                    endl << list[i].masuk.tanggal << " " << list[i].masuk.bulan << " " << list[i].masuk.tahun <<
+                    endl << list[i].keluar.tanggal << " " << list[i].keluar.bulan << " " << list[i].keluar.tahun << endl;
+                }
+                file.close();
+                check = false;
+                break;
+            default :
+                break;
+        }
+    }
+    delete[] list;
+    return 0;
+}
+
 void menu(){
-    cout << "==============================" <<
-    endl << "1. Tambahkan Tahanan" <<
-    endl << "2. Tampilkan sesuai urutan default" << 
-    endl << "3. Tampilkan sesuai urutan masuk" << 
-    endl << "4. Cari Tahanan" <<
-    endl << "5. Delete data tahanan" <<
-    endl << "6. Simpan dan keluar" << 
-    endl << "==============================\n";
+    cout << "========================================" <<
+    endl << "| 1. Tambahkan Tahanan                 |" <<
+    endl << "| 2. Tampilkan sesuai urutan default   |" << 
+    endl << "| 3. Tampilkan sesuai urutan masuk     |" << 
+    endl << "| 4. Cari Tahanan                      |" <<
+    endl << "| 5. Delete data tahanan               |" <<
+    endl << "| 6. Simpan dan keluar                 |" << 
+    endl << "========================================\n";
 }
 
 void input(Tahanan* list, int& jmlhTahanan){
@@ -41,6 +108,7 @@ void input(Tahanan* list, int& jmlhTahanan){
          >> list[jmlhTahanan - 1].masuk.bulan >> list[jmlhTahanan - 1].masuk.tahun;
     cout << "Tanggal Keluar\t: "; cin >> list[jmlhTahanan - 1].keluar.tanggal 
          >> list[jmlhTahanan - 1].keluar.bulan >> list[jmlhTahanan - 1].keluar.tahun;
+    return;
 }
 
 void hapus(Tahanan* list, int& jmlhTahanan){
@@ -63,51 +131,25 @@ void hapus(Tahanan* list, int& jmlhTahanan){
     else cout << "Data tahanan tersebut tidak ditemukan!!\n";
 }
 
-int main(){
-    int jmlhTahanan = 0;
-    fstream file;
-    Tahanan* list = new Tahanan[101];
-    Tahanan* ptr = &list[0];
-    file.open(namaFile);
-    file >> jmlhTahanan;
-    Tahanan* max = ptr + jmlhTahanan;
-    for (ptr; ptr < max; ptr++){
-        file >> ptr->nama >> ptr->noTahanan >> ptr->tmptLahir >> ptr->kejahatan 
-        >> ptr->masuk.tanggal >> ptr->masuk.bulan >> ptr->masuk.tahun 
-        >> ptr->keluar.tanggal >> ptr->keluar.bulan >> ptr->keluar.tahun;
-    }
-    file.close();
+void cari_tahanan(Tahanan* list, int& jmlhTahanan){
+    string input_id;
+    cout << "Masukkan ID tahanan : ";
+    cin.ignore();
+    getline(cin, input_id);
 
-    while (true){
-        int pilih;
-        menu();
-        cin >> pilih;
-        switch (pilih){
-            case 1 :
-                input(list, jmlhTahanan);
-                break;
-            case 2 :
-
-            case 3 :
-
-            case 4 :
-
-            case 5 :
-                hapus(list, jmlhTahanan);
-                break;
-            case 6 :
-                file.open(namaFile, ios::trunc);
-                for (int i = 0; i < jmlhTahanan; i++){
-                    file << list[i].nama <<
-                    endl << list[i].noTahanan << " " << list[i].tmptLahir << " " << list[i].kejahatan << 
-                    endl << list[i].masuk.tanggal << " " << list[i].masuk.bulan << " " << list[i].masuk.tahun <<
-                    endl << list[i].keluar.tanggal << " " << list[i].keluar.bulan << " " << list[i].keluar.tahun << endl;
-                }
-                file.close();
-                break;
-            
+    bool found = false;
+    for (int i = 0; i < jmlhTahanan; i++) {
+        
+        if (list[i].noTahanan == input_id) {
+            cout << list[i].noTahanan << endl <<
+                    list[i].nama << endl <<
+                    list[i].tmptLahir << endl <<
+                    list[i].kejahatan << endl <<
+                    list[i].masuk.tanggal << "-" << list[i].masuk.bulan << "-" << list[i].masuk.tahun << endl <<
+                    list[i].keluar.tanggal << "-" << list[i].keluar.bulan << "-" << list[i].keluar.tahun << endl;
+                    found = true;
         }
+        if (!found) cout << "Tahanan tidak ditemukan!\n"; 
+
     }
-    delete[] list;
-    return 0;
 }
